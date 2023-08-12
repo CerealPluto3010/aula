a5cce2e1f905f76a1b02cf162a4f3ae850f28299
Lucas Serafim <lucasserafim422@gmail.com>
2023-08-12 11:21:17 -0300

    Initial commit

---
+++ src/main/java/com/aula/aula/ClienteController.java
@@ -0,0 +1,40 @@
+package com.aula.aula;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+// Create (POST) Read (GET) Update (PUT) Delete (DELETE)
+@RestController
+@RequestMapping(path = "/cliente")
+public class ClienteController { // Model View Controller
+    private ClienteRepository clienteRepository;
+
+    public ClienteController(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+    @GetMapping // VERBO HTTP - GET
+    public Collection<Cliente> getNomeCliente() {
+        return clienteRepository.findAll();
+    }
+
+    @PostMapping
+    public Cliente criarCliente(@RequestBody Cliente cliente) {
+        System.out.println(cliente);
+        clienteRepository.save(cliente);
+        return cliente;
+    }
+    @PutMapping
+    public Cliente alterarCliente(@RequestBody Cliente cliente) {
+        System.out.println(cliente);
+        clienteRepository.save(cliente);
+        return cliente;
+    }
+
+    @DeleteMapping
+    public void removerCliente(@RequestBody Cliente cliente) {
+        System.out.println(cliente);
+        clienteRepository.delete(cliente);
+    }
+
+}
